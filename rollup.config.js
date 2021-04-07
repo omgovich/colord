9eed7b25d5d04d3d42f120aba43e335aea6a12f3
Vlad Shilov <omgovich@ya.ru>
2021-04-07 12:29:29 +0300

    Publish CJS for better Node.js support

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import path from "path";
 import glob from "glob";
 import typescript from "rollup-plugin-typescript2";
 import { terser } from "rollup-plugin-terser";
@@ -17,23 +18,47 @@ const getRollupPluginsConfig = (compilerOptions) => {
   ];
 };
 
+// Find available plugins
+const colordPluginPaths = glob.sync("./src/plugins/*.ts");
+
+// Bundle both formats according to NodeJS guide
+// https://nodejs.org/api/packages.html#packages_approach_2_isolate_state
 export default [
-  // Build the main bundle
+  // Build the main bundle in both ESM and CJS modules
   {
     input: "src/index.ts",
     output: {
-      dir: "dist",
+      file: "dist/index.mjs",
       format: "es",
     },
     plugins: getRollupPluginsConfig({ declaration: true }),
   },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.cjs",
+      format: "cjs",
+    },
+    plugins: getRollupPluginsConfig({ declaration: false }),
+  },
 
-  // Bundle all colord plugins
-  ...glob.sync("./src/plugins/*.ts").map((input) => ({
+  // Bundle all library plugins as ESM modules
+  ...colordPluginPaths.map((input) => ({
     input,
     output: {
-      dir: "dist/plugins",
+      file: `dist/plugins/${path.parse(input).name}.mjs`,
       format: "es",
+    },
+    plugins: getRollupPluginsConfig({ declaration: false }),
+  })),
+
+  // Bundle all library plugins as CommonJS modules
+  ...colordPluginPaths.map((input) => ({
+    input,
+    output: {
+      file: `dist/plugins/${path.parse(input).name}.cjs`,
+      format: "cjs",
+      exports: "default",
     },
     plugins: getRollupPluginsConfig({ declaration: false }),
   })),
