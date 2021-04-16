adc3c5ce90e8b3512dcbf36632e29494a50606d1
Vlad Shilov <omgovich@ya.ru>
2021-04-16 13:00:23 +0300

    Lighter ES5 code

--- src/manipulate/saturate.ts
+++ src/manipulate/saturate.ts
@@ -3,12 +3,12 @@ import { HslaColor, RgbaColor } from "../types";
 import { clamp } from "../helpers";
 
 export const saturate = (rgba: RgbaColor, amount: number): HslaColor => {
-  const { h, s, l, a } = rgbaToHsla(rgba);
+  const hsla = rgbaToHsla(rgba);
 
   return {
-    h,
-    s: clamp(s + amount * 100, 0, 100),
-    l,
-    a,
+    h: hsla.h,
+    s: clamp(hsla.s + amount * 100, 0, 100),
+    l: hsla.l,
+    a: hsla.a,
   };
 };
