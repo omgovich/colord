f22ca978246cad1a9e688d8fb525b1b748ae428e
Vlad Shilov <omgovich@ya.ru>
2021-07-23 14:58:46 +0300

    v2.4

--- src/plugins/harmonies.ts
+++ src/plugins/harmonies.ts
@@ -1,19 +1,19 @@
 import { Plugin } from "../extend";
 
-export type Harmony =
+export type HarmonyType =
   | "analogous"
-  | "complimentary"
+  | "complementary"
   | "rectangle"
+  | "split-complementary"
   | "tetradic"
-  | "triadic"
-  | "split-complimentary";
+  | "triadic";
 
 declare module "../colord" {
   interface Colord {
     /**
      * Returns an array of harmony colors as `Colord` instances.
      */
-    harmonies(type: Harmony): Colord[];
+    harmonies(type?: HarmonyType): Colord[];
   }
 }
 
@@ -25,16 +25,16 @@ const harmoniesPlugin: Plugin = (ColordClass): void => {
   /**
    * Harmony colors are colors with particular hue shift of the original color.
    */
-  const hueShifts: Record<Harmony, number[]> = {
+  const hueShifts: Record<HarmonyType, number[]> = {
     analogous: [-30, 0, 30],
-    complimentary: [0, 180],
+    complementary: [0, 180],
     rectangle: [0, 60, 180, 240],
     tetradic: [0, 90, 180, 270],
     triadic: [0, 120, 240],
-    "split-complimentary": [0, 150, 210],
+    "split-complementary": [0, 150, 210],
   };
 
-  ColordClass.prototype.harmonies = function (type = "complimentary") {
+  ColordClass.prototype.harmonies = function (type = "complementary") {
     return hueShifts[type].map((shift) => this.rotate(shift));
   };
 };
